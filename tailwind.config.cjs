c686b6c72ff250768bfb93c5b391d91757e3ffa3
john-mirage <chaosdynamix@posteo.net>
2022-10-10 15:27:56 +0200

    feat: add tailwind default conf and ignore tailwind for linter and formatter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,67 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      "xs": "375px",
+      "sm": "576px",
+      "md": "768px",
+      "lg": "992px",
+      "xl": "1200px",
+      "2xl": "1400px",
+    },
+    colors: {
+      transparent: "transparent",
+    },
+    spacing: {
+      "0": "0px",
+      "1": "0.0625rem",
+      "2": "0.125rem",
+      "4": "0.25rem",
+      "8": "0.5rem",
+      "12": "0.75rem",
+      "16": "1rem",
+      "20": "1.25rem",
+      "24": "1.5rem",
+      "28": "1.75rem",
+      "32": "2rem",
+      "36": "2.25rem",
+      "40": "2.5rem",
+      "44": "2.75rem",
+      "48": "3rem",
+      "52": "3.25rem",
+      "56": "3.5rem",
+      "60": "3.75rem",
+      "64": "4rem",
+      "68": "4.25rem",
+      "72": "4.5rem",
+      "76": "4.75rem",
+      "80": "5rem",
+      "84": "5.25rem",
+      "88": "5.5rem",
+      "92": "5.75rem",
+      "96": "6rem",
+      "100": "6.25rem",
+      "128": "8rem",
+      "256": "16rem",
+      "512": "32rem",
+      "1024": "64rem",
+    },
+    fontWeight: {
+      "300": "300",
+      "400": "400",
+      "500": "500",
+      "600": "600",
+      "700": "700",
+      "800": "800",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["font name", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
